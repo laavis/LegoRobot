368dfafd5c4d3ae13d1862e29e7c0d593586b34d
Alex Mäkinen <alex.makinen@metropolia.fi>
2018-04-19 12:50:03 +0300

    helloo

--- src/pack/DistanceIR.java
+++ src/pack/DistanceIR.java
@@ -5,30 +5,30 @@ import lejos.hardware.sensor.EV3IRSensor;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
-public class DistanceIR {
+public class DistanceIR extends Thread {
+	private EV3IRSensor infraredSensorRight;
 
-	private static EV3IRSensor ir1 = new EV3IRSensor(SensorPort.S4);
+	// private static EV3IRSensor ir1 = new EV3IRSensor(SensorPort.S4);
+
+	public DistanceIR(EV3IRSensor sensorRight) {
+		this.infraredSensorRight = sensorRight;
+	}
 
 	private static int HALF_SECOND = 500;
 
-	public static void main(String[] args) {
+	public int getDistance() {
 
-		final SampleProvider sp = ir1.getDistanceMode();
+		final SampleProvider sp = infraredSensorRight.getDistanceMode();
 		int distanceValue = 0;
 
 		// Control loop
-		final int iteration_threshold = 50;
-		for (int i = 0; i <= iteration_threshold; i++) {
 
-			float[] sample = new float[sp.sampleSize()];
-			sp.fetchSample(sample, 0);
-			distanceValue = (int) sample[0];
+		float[] sample = new float[sp.sampleSize()];
+		sp.fetchSample(sample, 0);
+		distanceValue = (int) sample[0];
 
-			System.out.println("Iteration: {}" + i);
-			System.out.println("Distance: {}" + distanceValue);
-
-			Delay.msDelay(HALF_SECOND);
-		}
+		Delay.msDelay(HALF_SECOND);
+		return distanceValue;
 
 	}
 
