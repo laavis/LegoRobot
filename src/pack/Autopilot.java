ab5e927a413e5d14f214cf43b4e078b4d0fc01cf
laavis <sara.suviranta@gmail.com>
2018-04-26 13:19:05 +0300

    more autopilot stuff

--- src/pack/Autopilot.java
+++ src/pack/Autopilot.java
@@ -8,6 +8,7 @@ public class Autopilot extends Thread {
 	private Drive dr;
 	private DistanceIR distance;
 	private boolean stop = false;
+	private int shootCount = 0;
 
 	public Autopilot(Drive dr, DistanceIR distance) {
 		this.dr = dr;
@@ -18,10 +19,14 @@ public class Autopilot extends Thread {
 		try {
 			while (!Button.ESCAPE.isDown()) {
 				LCD.drawString("AUTOPILOT", 0, 0);
+				LCD.drawString("Distance: " + distance.distance(), 0, 6);
+				LCD.drawString("Shoot count: " + shootCount, 0, 5);
+				LCD.refresh();
+				//LCD.clear();
 				Thread.sleep(25);
 				dr.driveForward();
 				detectObstacle();
-				shoot();
+				
 			}
 		} catch (Exception e) {
 			return;
@@ -29,19 +34,20 @@ public class Autopilot extends Thread {
 	}
 	// polls distance sensor for information and shoots or turns around accordingly
 	private void detectObstacle() {
-		if (distance.distance() <= 30) {
+		if (distance.distance() <= 40 && shootCount > 0) {
 			dr.turnLeft90();
 			LCD.drawString("DETECT", 0, 2);
+			shootCount = 0;
+		} else if (distance.distance() <= 100 && distance.distance() > 41 && shootCount == 0) {
+			kill();
+			shootCount++;
 		}
 		LCD.clear(2);
 	}
 	
-	private void shoot() {
-		if (distance.distance() <= 100 && distance.distance() > 31) {
-			dr.stop();
-			dr.shoot();
-			LCD.drawString("SHOOT", 0, 3);
-		}
+	private void kill() {
+		dr.stop();
+		dr.shoot();
 		LCD.clear(3);
 	}
 
