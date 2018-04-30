a4521c04b375353203693671610475a041484c24
Miikka Oksanen <miikka.oksanen@pp1.inet.fi>
2018-04-30 13:41:58 +0300

    FINALLY, manual music stop works

---
+++ src/pack/MusicThread.java
@@ -0,0 +1,225 @@
+package pack;
+
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
+
+public class MusicThread extends Thread {
+	
+	public String song = null;
+	public final static int[] PIANO = new int[]{4, 25, 500, 7000, 5};
+	
+	public void run() {
+		try {
+			while (!Button.ENTER.isDown()) {					
+				if (song != null) {
+					Sound.setVolume(10);
+					LCD.drawString("Playing " + song, 0, 0);
+					PlaySong(song);
+				}
+			}
+		} catch (Exception e) {
+			return;
+		}
+		
+	}
+		
+	
+	public void PlayNote(char note, int howManyTimes, double forHowLong, int scale) {
+		
+		int frequency = 0;
+		
+		switch (note) {
+		case 'a':
+			if (scale == 4) {
+				frequency = 440;
+			}
+			else if (scale == 5) {
+				frequency = 880;
+			}
+			break;
+		case 'b':
+			if (scale == 4) {
+				frequency = 494;
+			}
+			else if (scale == 5) {
+				frequency = 988;
+			}
+			break;
+		case 'c':
+			if (scale == 4) {
+				frequency = 262;
+			}
+			else if (scale == 5) {
+				frequency = 523;
+			}
+			break;
+		case 'd':
+			if (scale == 4) {
+				frequency = 294;
+			}
+			else if (scale == 5) {
+				frequency = 587;
+			}
+			break;
+		case 'e':
+			if (scale == 4) {
+				frequency = 330;
+			}
+			else if (scale == 5) {
+				frequency = 659;
+			}
+			break;
+		case 'f':
+			if (scale == 4) {
+				frequency = 349;
+			}
+			else if (scale == 5) {
+				frequency = 698;
+			}
+			break;
+		case 'g':
+			if (scale == 4) {
+				frequency = 392;
+			}
+			else if (scale == 5) {
+				frequency = 784;
+			}
+			break;
+		}
+		
+		int i = 0;
+		while (i < howManyTimes && !Button.ENTER.isDown()) {
+			Sound.playNote(PIANO, frequency, (int)Math.round(300 * forHowLong));
+			i++;
+		}
+	}
+	
+	
+	public void PlaySong(String song) {
+		if (song.equals("Ukko")) {
+			//PHASE 1			
+			PlayNote('c', 3, 1, 4);
+			PlayNote('e', 1, 1, 4);
+			PlayNote('d', 3, 1, 4);
+			PlayNote('f', 1, 1, 4);
+			PlayNote('e', 2, 1, 4);
+			PlayNote('d', 2, 1, 4);
+			PlayNote('c', 1, 4, 4);
+			//END OF PHASE 1
+			
+			//PHASE 2
+			PlayNote('e', 4, 1, 4);
+			PlayNote('g', 1, 2, 4);
+			PlayNote('f', 1, 2, 4);
+			PlayNote('d', 4, 1, 4);
+			PlayNote('f', 1, 2, 4);
+			PlayNote('e', 1, 2, 4);
+			//END OF PHASE 2
+			
+			//PHASE 3
+			PlayNote('c', 3, 1, 4);
+			PlayNote('e', 1, 1, 4);
+			PlayNote('d', 3, 1, 4);
+			PlayNote('f', 1, 1, 4);
+			PlayNote('e', 2, 1, 4);
+			PlayNote('d', 2, 1, 4);
+			PlayNote('c', 1, 4, 4);
+			//END OF PHASE 3
+		}
+		
+		if (song.equals("Shelter")) {
+			//PHASE 1
+			PlayNote('c', 1, 0.75, 5);
+			PlayNote('a', 1, 0.19, 5);
+			PlayNote('g', 1, 0.375, 5);
+			PlayNote('c', 1, 1.6875, 5);
+			//END OF PHASE 1
+			
+			//PHASE 2
+			PlayNote('c', 1, 0.75, 5);
+			PlayNote('a', 1, 0.19, 5);
+			PlayNote('g', 1, 0.375, 5);
+			PlayNote('h', 1, 0.9375, 4);
+			PlayNote('d', 1, 0.375, 5);
+			PlayNote('c', 1, 0.375, 5);
+			//END OF PHASE 2
+			
+			//PHASE 3
+			PlayNote('c', 1, 0.75, 5);
+			PlayNote('a', 1, 0.19, 5);
+			PlayNote('g', 1, 0.375, 5);
+			PlayNote('c', 1, 1.3125, 5);
+			PlayNote('e', 1, 0.375, 5);
+			//END OF PHASE 3
+			
+			//PHASE 4
+			PlayNote('e', 1, 0.25, 5);
+			PlayNote('c', 1, 0.25, 5);
+			PlayNote('d', 1, 0.25, 5);
+			
+			PlayNote('e', 1, 0.25, 5);
+			PlayNote('c', 1, 0.25, 5);
+			PlayNote('d', 1, 0.25, 5);
+			
+			PlayNote('e', 1, 0.25, 5);
+			PlayNote('c', 1, 0.25, 5);
+			PlayNote('d', 1, 0.25, 5);
+			
+			PlayNote('e', 1, 0.25, 5);
+			PlayNote('c', 1, 0.25, 5);
+			PlayNote('d', 1, 0.25, 5);
+			//END OF PHASE 4
+			
+			//PHASE 5
+			PlayNote('c', 1, 0.75, 5);
+			PlayNote('a', 1, 0.19, 5);
+			PlayNote('g', 1, 0.375, 5);
+			PlayNote('c', 1, 1.6875, 5);
+			//END OF PHASE 5
+			
+			//PHASE 6
+			PlayNote('c', 1, 0.75, 5);
+			PlayNote('a', 1, 0.19, 5);
+			PlayNote('g', 1, 0.375, 5);
+			PlayNote('d', 1, 0.9375, 5);
+			PlayNote('e', 1, 0.375, 5);
+			PlayNote('g', 1, 0.375, 5);
+			//END OF PHASE 6
+			
+			//PHASE 7
+			PlayNote('g', 1, 0.75, 5);
+			PlayNote('a', 1, 0.19, 5);
+			PlayNote('g', 1, 0.375, 5);
+			PlayNote('c', 1, 1.3125, 5);
+			PlayNote('e', 1, 0.375, 5);
+			//END OF PHASE 7
+			
+			//PHASE 8
+			PlayNote('e', 1, 0.25, 5);
+			PlayNote('c', 1, 0.25, 5);
+			PlayNote('d', 1, 0.25, 5);
+			
+			PlayNote('e', 1, 0.25, 5);
+			PlayNote('c', 1, 0.25, 5);
+			PlayNote('d', 1, 0.25, 5);
+			
+			PlayNote('e', 1, 0.25, 5);
+			PlayNote('c', 1, 0.25, 5);
+			PlayNote('d', 1, 0.25, 5);
+			
+			PlayNote('e', 1, 0.25, 5);
+			PlayNote('c', 1, 0.25, 5);
+			PlayNote('d', 1, 0.25, 5);
+			PlayNote('f', 1, 0.75, 4);
+			//END OF PHASE 8
+			
+		}
+	}
+	
+	
+	public void setSong(String song) {
+		this.song = song;
+	}
+
+}
